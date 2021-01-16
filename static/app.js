ebb1184d7a23efa0646e3f0d8fc25f049508c76e
maitenina <leslignesdemaina@gmail.com>
2021-01-16 01:54:09 +0100

    add background fix bottom background issu and font size

--- static/app.js
+++ static/app.js
@@ -14,6 +14,8 @@ async function main(){
       const meteo = await fetch("https://api.openweathermap.org/data/2.5/weather?&lang=fr&units=metric&appid=f0cd26ef432d833785e4455713d29d81&q=" + ville)
   .then(resultat => resultat.json())
   .then(json => json)
+  console.log(meteo);
+
 
 
   displayWeatherInfos(meteo);
@@ -29,9 +31,11 @@ const icons = {
   "Clouds": "wi wi-day-cloudy",
   "Clear": "wi wi-day sunny",
   "Snow": "wi wi-day-snow",
-  "mist": "wi wi-day-fog",
+  "Fog": "wi wi-day-fog",
   "Drizzle": "wi wi-day-sleet",
 }
+
+
 
   //utilisation des données récoltées pour mettre à jour la page
 function displayWeatherInfos(data){
@@ -41,11 +45,15 @@ const temperature = data.main.temp;
 const conditions = data.weather[0].main;
 const description = data.weather[0].description;
 
+//attribue les données dans contenu du site
+
 document.querySelector("#city").textContent = name;
 document.querySelector("#temperature").textContent = Math.round(temperature);
 document.querySelector("#weather-description").textContent = capitalize(description);
 
 document.querySelector("i.wi").className = icons[conditions];
+
+document.body.className = conditions.toLowerCase();
 }
 
 main();
